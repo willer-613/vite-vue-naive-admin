e5cb11d61046c870d0c64f4dcc5f0e2eacd81394
willer613 <lb4013@126.com>
2022-05-15 14:47:25 +0800

    feat: 修改docker sh
    修改docker sh

--- vite-vue-naive-admin-deploy.sh
+++ vite-vue-naive-admin-deploy.sh
@@ -2,7 +2,7 @@ echo -e "---------docker Login--------"
 
 echo -e $1 $2
 
-docker login --username=$1 registry.cn-shanghai.aliyuncs.com --password=$2
+docker login --username=$1 registry.cn-shanghai.aliyuncs.com --password=$2 --password-stdin
 echo -e "---------docker Stop--------"
 docker stop vite-vue-naive-admin
 echo -e "---------docker Rm--------"
