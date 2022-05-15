1c22b63bfbb2427e06903c32a810c71f8a6fdd2f
willer613 <lb4013@126.com>
2022-05-15 13:12:23 +0800

    fix: 修改sh
    修改sh

--- vite-vue-naive-admin-deploy.sh
+++ vite-vue-naive-admin-deploy.sh
@@ -1,12 +1,12 @@
 echo -e "---------docker Login--------"
-docker login --username=$1 registry.cn-hangzhou.aliyuncs.com --password=$2
+docker login --username=$1 registry.cn-shanghai.aliyuncs.com --password=$2
 echo -e "---------docker Stop--------"
-docker stop bilibili-vue
+docker stop vite-vue-naive-admin
 echo -e "---------docker Rm--------"
-docker rm bilibili-vue
-docker rmi registry.cn-hangzhou.aliyuncs.com/zlyyyy/bilibili-vue:latest
+docker rm vite-vue-naive-admin
+docker rmi registry.cn-shanghai.aliyuncs.com/willer-docker/vite-vue-naive-admin:latest
 echo -e "---------docker Pull--------"
-docker pull registry.cn-hangzhou.aliyuncs.com/zlyyyy/bilibili-vue:latest
+docker pull registry.cn-shanghai.aliyuncs.com/willer-docker/vite-vue-naive-admin:latest
 echo -e "---------docker Create and Start--------"
-docker run --rm -d -p 8081:80 --name bilibili-vue registry.cn-hangzhou.aliyuncs.com/zlyyyy/bilibili-vue:latest
+docker run --rm -d -p 8081:80 --name vite-vue-naive-admin registry.cn-shanghai.aliyuncs.com/willer-docker/vite-vue-naive-admin:latest
 echo -e "---------deploy Success--------"
